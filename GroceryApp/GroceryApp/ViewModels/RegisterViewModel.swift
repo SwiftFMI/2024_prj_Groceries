7a3cd066da138e0e6046c3f54f2ff3e1f1131b31
zlatinalilova <zlati.lilova@gmail.com>
2025-02-18 21:07:47 +0200

    Improved registerView

--- GroceryApp/GroceryApp/ViewModels/RegisterViewModel.swift
+++ GroceryApp/GroceryApp/ViewModels/RegisterViewModel.swift
@@ -12,11 +12,60 @@ import SwiftUI
     private lazy var auth = FirebaseAuth()
     
     
-    @Published var email = ""
     @Published var password = ""
+    @Published var confirmPass = ""
+    @Published var isPassValid = true
+    
+    @Published var arePasswordsMatching = true
+    @Published var passErrorMessage = ""
+    @Published var confirmPassErrorMessage = ""
     
     @Published var errorOnRegister = false
     @Published var errorText = "Error while registering"
+    
+    @Published var email = ""
+
+    @Published var isEmailValid = true
+    @Published var emailErrorMessage = ""
+    
+    func validateEmail(email: String) {
+        let emailRegex = #"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$"#
+        let predicate = NSPredicate(format: "SELF MATCHES[c] %@", emailRegex)
+        isEmailValid = predicate.evaluate(with: email)
+
+        emailErrorMessage = isEmailValid ? "" : "Invalid email format"
+    }
+    
+    func validatePass(pass: String){
+        if pass.count < 6 {
+            isPassValid = false
+            passErrorMessage = "The password must be at least 6 characters"
+            return
+        }
+        else if !pass.contains(where: { ch in ch.isNumber }) {
+            isPassValid = false
+            passErrorMessage = "The password must contain at least 1 number"
+            return
+        }
+        
+        isPassValid = true
+        
+        passErrorMessage = isPassValid ? "" : "Invalid password format"
+        
+    }
+    
+    func passwordsMatch(confirmPass: String){
+        if password != confirmPass {
+            arePasswordsMatching = false
+            confirmPassErrorMessage = "The passwords don't match"
+            return
+        }
+        
+        arePasswordsMatching = true
+        confirmPassErrorMessage = arePasswordsMatching ? "" :"The passwords don't match"
+        
+    }
+    
     
     func register() {
         auth.register(email: email, password: password) { result in
@@ -30,4 +79,6 @@ import SwiftUI
             }
         }
     }
+    
+    
 }
--- GroceryApp/GroceryApp/Views/RegisterView.swift
+++ GroceryApp/GroceryApp/Views/RegisterView.swift
@@ -1,35 +1,133 @@
+// RegisterView.swift
+// GroceryApp
 //
-//  RegisterView.swift
-//  GroceryApp
-//
-//  Created by Zlatina Lilova on 16.02.25.
+// Created by Zlatina Lilova on 16.02.25.
 //
 import SwiftUI
 
 struct RegisterView: View {
     
-    @State var vm: RegisterViewModel = RegisterViewModel()
+    @StateObject var vm: RegisterViewModel = RegisterViewModel()
     
     var body: some View {
-        Form {
-            Section{
-                TextField("Email", text: $vm.email)
-            }
-            Section{
-                TextField("Password", text: $vm.password)
-            }
-            
-            Section{
-                Button {
-                    vm.register()
-                } label: {
-                    Text("Register")
-                }
-                .alert("Error while registering", isPresented: $vm.errorOnRegister) {
-                    Button("OK", role: .cancel) { }
-                }
+        
+        VStack(spacing: 24) {
+            Text("Register")
+               .font(.system(size: 50))
+               .fontWeight(.heavy)
+               .padding(.top)
 
+            EmailTextView(
+                email: $vm.email,
+                isEmailValid: $vm.isEmailValid,
+                validateEmail: { email in
+                    vm.validateEmail(email: email)
+                },
+                emailErrorMessage: $vm.emailErrorMessage
+            )
+            
+            PasswordTextView(
+                pass: $vm.password,
+                isPassValid: $vm.isPassValid,
+                validatePass: { pass in
+                    vm.validatePass(pass: pass)
+                },
+                passErrorMessage: $vm.passErrorMessage,
+                confirmPass: false
+            )
+            
+            PasswordTextView(
+                pass: $vm.confirmPass,
+                isPassValid: $vm.arePasswordsMatching,
+                validatePass: { pass in
+                    vm.passwordsMatch(confirmPass: pass)
+                },
+                passErrorMessage: $vm.confirmPassErrorMessage,
+                confirmPass: true
+            )
+            
+            Spacer()
+            
+            Button(action: {
+                vm.register()
+            }) {
+                Text("Register")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .font(.headline)
+            }
+            .alert("Error while registering", isPresented: $vm.errorOnRegister) {
+                Button("OK", role: .cancel) { }
             }
         }
+        .padding(.horizontal, 24)
+        .padding(.top, 32)
+    }
+}
+
+
+struct EmailTextView: View {
+    
+    @Binding var email: String
+    @Binding var isEmailValid : Bool
+    var validateEmail: (_ email: String) -> Void
+    @Binding var emailErrorMessage : String
+    
+    var body : some View {
+        VStack(alignment: .leading, spacing: 8) {
+            TextField("Email", text: $email)
+                .onChange(of: email, perform: { newValue in
+                    validateEmail(newValue)
+                })
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(!isEmailValid ? Color.red : Color(.systemBlue), lineWidth: 1))
+            
+            if !isEmailValid {
+                Text(emailErrorMessage)
+                    .foregroundColor(.red)
+                    .font(.footnote)
+            }
+        }
+        .padding(.bottom, 16)
+    }
+}
+
+struct PasswordTextView: View {
+    
+    @Binding var pass: String
+    @Binding var isPassValid : Bool
+    var validatePass: (_ pass: String) -> Void
+    @Binding var passErrorMessage : String
+    let confirmPass: Bool
+    
+    var text: String {
+        confirmPass ? "Confirm password" : "Password"
+    }
+    
+    var body : some View {
+        VStack(alignment: .leading, spacing: 8) {
+            SecureField(text, text: $pass)
+                .onChange(of: pass, perform: { newValue in
+                    validatePass(newValue)
+                })
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(!isPassValid ? Color.red : Color(.systemBlue), lineWidth: 1))
+            
+            if !isPassValid {
+                Text(passErrorMessage)
+                    .foregroundColor(.red)
+                    .font(.footnote)
+            }
+        }
+        .padding(.bottom, 16)
     }
 }
