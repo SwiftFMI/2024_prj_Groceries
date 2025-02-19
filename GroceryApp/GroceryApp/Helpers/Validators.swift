886d801dc5d4e5f8eed715944fc3e2d395c6d5e2
NikolayDinkov <48023914+NikolayDinkov@users.noreply.github.com>
2025-02-19 19:33:03 +0200

    Validator update

--- GroceryApp/GroceryApp/Helpers/Validators.swift
+++ GroceryApp/GroceryApp/Helpers/Validators.swift
@@ -7,18 +7,21 @@
 
 import SwiftUI
 
-class Validators {
-    func validatePass(pass: String) -> (Bool, String){
+final class Validators {
+
+    private init() {}
+
+    static func validatePass(pass: String) -> (Bool, String){
         if pass.count < 6 {
-            return (false,"The password must be at least 6 characters")
+            return (false, "The password must be at least 6 characters")
         }
         else if !pass.contains(where: { ch in ch.isNumber }) {
-            return (false,"The password must contain at least 1 number")
+            return (false, "The password must contain at least 1 number")
         }
-        return (true,"")
+        return (true, "")
     }
     
-    func validateEmail(email: String) -> (Bool, String){
+    static func validateEmail(email: String) -> (Bool, String){
         let emailRegex = #"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$"#
         let predicate = NSPredicate(format: "SELF MATCHES[c] %@", emailRegex)
         let isEmailValid = predicate.evaluate(with: email)
@@ -28,11 +31,11 @@ class Validators {
         return(isEmailValid, emailErrorMessage)
     }
     
-    func validateUsername(userName: String) -> (Bool, String){
+    static func validateUsername(userName: String) -> (Bool, String){
         if userName.count < 3 {
-            return (false,"The username must be at least 3 characters")
+            return (false, "The username must be at least 3 characters")
         }
 
-        return (true,"")
+        return (true, "")
     }
 }
