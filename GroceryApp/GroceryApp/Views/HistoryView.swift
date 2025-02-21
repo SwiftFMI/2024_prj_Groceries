442329bece0fe5f59c05adeeb8020e7326946f5f
NikolayDinkov <48023914+NikolayDinkov@users.noreply.github.com>
2025-02-21 17:51:03 +0200

    Update sorting in cart history

--- GroceryApp/GroceryApp/Views/HistoryView.swift
+++ GroceryApp/GroceryApp/Views/HistoryView.swift
@@ -7,33 +7,33 @@
 import SwiftUI
 
 struct HistoryView: View {
-    
+
     @ObservedObject var viewModel: HistoryViewModel
-    
+
     var body: some View {
-            VStack{
-                    List(viewModel.lists, id: \.id) { list in
-                        HStack {
-                            Image(systemName: "list.clipboard.fill")
-                                .foregroundColor(.primary)
-                            
-                            Text("Shopping list: \(list.date, formatter: viewModel.dateFormatter)")
-                                .fontWeight(.semibold)
-                                .foregroundColor(.primary)
-                            
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray)
-                            
-                        }
-                        .onTapGesture { _ in
-                            viewModel.toDetail(list)
-                        }
-                        
-                    }
-                }.task {
-                    await viewModel.fetchHistoryLists()
+        VStack{
+            List(viewModel.lists.sorted(by: { $0.date > $1.date }), id: \.id) { list in
+                HStack {
+                    Image(systemName: "list.clipboard.fill")
+                        .foregroundColor(.primary)
+
+                    Text("Shopping list: \(list.date, formatter: viewModel.dateFormatter)")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.primary)
+
+                    Spacer()
+
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+
                 }
+                .onTapGesture { _ in
+                    viewModel.toDetail(list)
+                }
+                
             }
+        }.task {
+            await viewModel.fetchHistoryLists()
         }
+    }
+}
